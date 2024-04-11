a066bea735edb1064152b5062a309eee3dcefc55
Bram Enning <bramenning@gmail.com>
2024-04-11 09:56:11 +0200

    removed 'convert' statement from docker command
    removed 'convert' statement from docker command, so other subcommands of the eencijfer-package are also accessible (convert and create-assets).
    
    For the end-user the following command will work:
    - zsh /Users/bramenning/GitHub/docker_1cho/eencijfer_flag.sh convert -C --export-format parquet

--- eencijfer_flag.sh
+++ eencijfer_flag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t my_python_app .
-docker run -it --name my_running_app -v $(pwd):/data:rw my_python_app eencijfer convert "$@"
+docker run -it --name my_running_app -v $(pwd):/data:rw my_python_app eencijfer "$@"
--- eencijfer_flags.bat
+++ eencijfer_flags.bat
@@ -3,4 +3,4 @@
 docker build -t my_python_app .
 REM docker run -it --name my_running_app -v %cd%:/data:rw my_python_app sh -c "eencijfer init && eencijfer convert --export-format csv"
 
-docker run -it --name my_running_app -v %cd%:/data:rw my_python_app eencijfer convert %*
+docker run -it --name my_running_app -v %cd%:/data:rw my_python_app eencijfer %*
