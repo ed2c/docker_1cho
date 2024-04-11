a20915adc0dcad974a1aa8c646212f626c85325f
tin900 <113692500+tin900@users.noreply.github.com>
2024-04-11 10:06:06 +0200

    feat: stop mage and prune it

--- eencijfer_flag.sh
+++ eencijfer_flag.sh
@@ -2,3 +2,9 @@
 
 docker build -t my_python_app .
 docker run -it --name my_running_app -v $(pwd):/data:rw my_python_app eencijfer "$@"
+
+# Stop the container
+docker stop my_running_app
+
+# Prune the image
+docker image prune -f
