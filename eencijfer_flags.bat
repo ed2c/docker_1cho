cc5b38cd847d68736d781fea876b0c3cf9365c51
tin900 <t.iwan@vu.nl>
2024-04-16 12:43:35 +0200

    remove: scripts no longer needed

--- eencijfer_flags.bat
+++
@@ -1,6 +0,0 @@
-@echo on
-
-docker build -t my_python_app .
-REM docker run -it --name my_running_app -v %cd%:/data:rw my_python_app sh -c "eencijfer init && eencijfer convert --export-format csv"
-
-docker run -it --name my_running_app -v %cd%:/data:rw my_python_app eencijfer %*
